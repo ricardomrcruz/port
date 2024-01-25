6e3603e5dc424d4339c20e375285d33b39e4bdf3
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-25 23:07:20 +0100

    colorchanges

--- pages/fr.tsx
+++ pages/fr.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         <link rel="icon" href="/swsicon.png" />
       </Head>
 
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-trueGray-900 ">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-[#121212] ">
         <section className="min-h-screen">
           <nav className="py-10 mb-10 flex justify-between">
             <h1 className="text-base md:text-lg font-mono dark:text-white">
